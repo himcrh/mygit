4be1a641fab9cd269d3d13b2d63c81eb3400d4ab
himcrh <1178990376@qq.com>
2021-03-06 19:20:46 +0800

    acm1 G problem

---
+++ 2021上ACM算法与程序设计练习题1/G.cpp
@@ -0,0 +1,45 @@
+﻿#include<stdio.h>
+#include<algorithm>
+#include<string.h>
+using namespace std;
+int a[25][25];
+int main(){
+	int T;
+	scanf("%d",&T);
+	while(T--){
+		int n;
+		scanf("%d",&n);
+		memset(a,0,sizeof(a));
+		int x=n,y=1;
+		a[x][y]=1;
+		for(int i=2;i<=n*n;i++){
+			if(!a[x][y+1]&&y+1<=n){
+				a[x][y+1]=i;
+				y++;
+			}
+			else if(!a[x-1][y]&&x-1>=1){
+				a[x-1][y]=i;
+				x--;
+			}
+			else if(!a[x][y-1]&&y-1>=1){
+				a[x][y-1]=i;
+				y--;
+			}
+			else {
+				while(!a[x+1][y]){
+					a[x+1][y]=i;
+					x++;
+					i++;
+				}
+				i--;
+			}
+		}
+		for(int i=1;i<=n;i++){
+			for(int j=1;j<=n;j++){
+				printf("%4d",a[j][i]);
+			}
+			puts("");
+		}
+	}
+	return 0;
+}
